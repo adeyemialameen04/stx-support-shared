e01accd37d54dc1214d036186d008e0ca9c9952a
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-10-03 03:31:59 +0100

    feat: fixed index.ts

--- index.ts
+++ index.ts
@@ -1,4 +1,7 @@
-export const add = (a: number, b: number) => {
-  return a + b;
-};
+//   return a + b;
+// export const add = (a: number, b: number) => {
+// };
+//
 
+import { insertUserSchema, selectUserSchema } from "../src/db/schema/user";
+export { insertUserSchema, selectUserSchema };
